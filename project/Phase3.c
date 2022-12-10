5b990a8f84e1cb4dbc703130c2526ced1a7dc476
saleh <saleh.govahi@gmail.com>
2022-12-10 18:45:55 +0330

    Add a variable & a do while & if condition to end the game if it ended

--- project/Phase3.c
+++ project/Phase3.c
@@ -7,7 +7,7 @@ int main()
     int input,temp=1;
     int i,j,x,y;
     int battlesNumber = 0;
-    int counter = 0;
+    int counterBattlesRemaining = 0;
     int xBattle , yBattle;
     int xAttack , yAttack;
     char direction;
@@ -98,6 +98,7 @@ int main()
     //Part 7(Getting number of battles)
     printf("\nInput number of battles: ");
     scanf("%d" , &battlesNumber);
+    counterBattlesRemaining= battlesNumber*3;
 
     //Part 8(Getting coordinates & direction of player1 battles)
     printf("\nTurn of Player1\n");
@@ -150,70 +151,139 @@ int main()
 
     printf("\n-------------------------------------------------\n");
 
-    printf("\nPlayer1 | Try your luck\n");
-    scanf("%d %d",&xAttack , &yAttack);
+    do {
+        if (counterBattlesRemaining != 0){
+        //Attacking player 1
+        printf("\nPlayer1 | Try your luck\n\n");
+        scanf("%d %d",&yAttack , &xAttack);
+        printf("\n");
 
-    if (board2[xAttack][yAttack]==254){
-        board2[xAttack][yAttack]=42;
-        for(j=0 ; j<input+1 ; j++){
-            for(i=0 ; i<input+1 ; i++){
-                if((i==1) && (j<10)){
-                    printf(" ");
-                }
-                if((i != 0)||(j != 0)){
-                    if(board2[i][j]== 0 || board2[i][j]==254 ){
-                        if(i>=10){
-                            printf(" ");
+        if (board2[xAttack][yAttack]==254){
+            board2[xAttack][yAttack]=42;
+            for(j=0 ; j<input+1 ; j++){
+                for(i=0 ; i<input+1 ; i++){
+                    if((i==1) && (j<10)){
+                        printf(" ");
+                    }
+                    if((i != 0)||(j != 0)){
+                        if(board2[i][j]== 0 || board2[i][j]==254 ){
+                            if(i>=10){
+                                printf(" ");
+                            }
+                            printf("~ ");
                         }
-                        printf("~ ");
+                        else if(board2[i][j]==42){
+                            printf("%c ",board2[i][j]);
+                        }
+                        else {
+                            printf("%d ", board2[i][j]);
+                        }
                     }
-                    else if(board2[i][j]==42){
-                        printf("%c ",board2[i][j]);
-                    }
-                    else {
-                        printf("%d ", board2[i][j]);
+                    else{
+                        printf("  ");
                     }
                 }
-                else{
-                  printf("  ");
-                }
+                printf("\n");
+            }
+            counterBattlesRemaining--;
+        }
+        else{
+            board2[xAttack][yAttack]=158;
+            for(j=0 ; j<input+1 ; j++){
+                for(i=0 ; i<input+1 ; i++){
+                    if((i==1) && (j<10)){
+                        printf(" ");
+                    }
+                    if((i != 0)||(j != 0)){
+                        if(board2[i][j]== 0 || board2[i][j]==254 ){
+                            if(i>=10){
+                                printf(" ");
+                            }
+                            printf("~ ");
+                        }
+                        else if(board2[i][j]==158){
+                            printf("%c ",board2[i][j]);
+                        }
+                        else {
+                            printf("%d ", board2[i][j]);
+                        }
+                    }
+                    else{
+                        printf("  ");
+                    }
+              }
+              printf("\n");
+          }
+          board2[xAttack][yAttack]=0;
+      }
+      }
 
+      //Attacking player 2
+      if (counterBattlesRemaining != 0){
+      printf("\nPlayer2 | Try your luck\n\n");
+      scanf("%d %d",&yAttack , &xAttack);
+      printf("\n");
+
+      if (board1[xAttack][yAttack]==254){
+          board1[xAttack][yAttack]=42;
+          for(j=0 ; j<input+1 ; j++){
+              for(i=0 ; i<input+1 ; i++){
+                  if((i==1) && (j<10)){
+                      printf(" ");
+                  }
+                  if((i != 0)||(j != 0)){
+                      if(board1[i][j]== 0 || board1[i][j]==254 ){
+                          if(i>=10){
+                              printf(" ");
+                          }
+                          printf("~ ");
+                      }
+                      else if(board1[i][j]==42){
+                          printf("%c ",board1[i][j]);
+                      }
+                      else {
+                          printf("%d ", board1[i][j]);
+                      }
+                  }
+                  else{
+                      printf("  ");
+                  }
+              }
+              printf("\n");
+          }
+          counterBattlesRemaining--;
+      }
+      else{
+          board1[xAttack][yAttack]=158;
+          for(j=0 ; j<input+1 ; j++){
+              for(i=0 ; i<input+1 ; i++){
+                  if((i==1) && (j<10)){
+                      printf(" ");
+                  }
+                  if((i != 0)||(j != 0)){
+                      if(board1[i][j]== 0 || board1[i][j]==254 ){
+                          if(i>=10){
+                              printf(" ");
+                          }
+                          printf("~ ");
+                      }
+                      else if(board1[i][j]==158){
+                          printf("%c ",board1[i][j]);
+                      }
+                      else {
+                          printf("%d ", board1[i][j]);
+                      }
+                  }
+                  else{
+                      printf("  ");
+                  }
             }
             printf("\n");
         }
+        board1[xAttack][yAttack]=0;
     }
-    else{
-        board2[xAttack][yAttack]=158;
-        for(j=0 ; j<input+1 ; j++){
-            for(i=0 ; i<input+1 ; i++){
-                if((i==1) && (j<10)){
-                    printf(" ");
-                }
-                if((i != 0)||(j != 0)){
-                    if(board2[i][j]== 0 || board2[i][j]==254 ){
-                        if(i>=10){
-                            printf(" ");
-                        }
-                        printf("~ ");
-                    }
-                    else if(board2[i][j]==158){
-                        printf("%c ",board2[i][j]);
-                    }
-                    else {
-                        printf("%d ", board2[i][j]);
-                    }
-                }
-                else{
-                  printf("  ");
-                }
-
-            }
-            printf("\n");
-        }
-        board2[xAttack][yAttack]=0;
     }
-
-
+    } while(counterBattlesRemaining != 0);
 
     return 0;
 }
